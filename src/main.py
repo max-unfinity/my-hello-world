b3a0eb3ae5bc8714c838eb84e5f6572f250a9fca
unfinity <poldersen2@gmail.com>
2022-12-23 01:31:54 +0600

    first

---
+++ src/main.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from art import tprint
+
+# load ENV variables for debug
+# has no effect in production
+load_dotenv("local.env")
+
+
+def main():
+    name = os.environ["context.userLogin"]
+    print("Hello World! This app is run by the user:")
+    tprint(name)
+
+
+if __name__ == "__main__":
+    main()
